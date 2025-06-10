2cd1c760eb397f7475de4ed1cf00abed01e8af27
statpileelkhayate <melkhayate61@gmail.com>
2025-06-10 20:01:52 +0200

    Chore: layout top padding

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout() {
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
         <Topbar />
-        <main className="flex-1 overflow-y-auto bg-muted/50 px-4 pt-0"><Outlet /></main>
+        <main className="flex-1 overflow-y-auto bg-muted/50 px-4 pt-2"><Outlet /></main>
       </div>
     </div>
   );
