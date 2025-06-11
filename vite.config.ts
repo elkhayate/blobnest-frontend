258aa664418cb7ad33ec9a3e6037cc65d70b573b
statpileelkhayate <melkhayate61@gmail.com>
2025-06-11 02:55:04 +0200

    Chore: solve 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,21 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'spa-fallback',
+      configureServer(server) {
+        server.middlewares.use('/api', (_, __, next) => next())
+        server.middlewares.use((req, _, next) => {
+          if (req.url && !req.url.includes('.') && req.url !== '/') {
+            req.url = '/index.html'
+          }
+          next()
+        })
+      }
+    }
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
